28b0ebab1b25e72c7cf1e8f87ec42aa03462d4c5
Saurav Timilsina <sauravtimilsina7@gmail.com>
2026-05-23 00:51:35 +0545

    Fix backend server export for Vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 const app = require("./app");
 const runtimeConfig = require("./config/runtime");
 
-app.listen(runtimeConfig.port, () => {
-  console.log(`Server is running on port ${runtimeConfig.port}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(runtimeConfig.port, () => {
+    console.log(`Server is running on port ${runtimeConfig.port}`);
+  });
+}
+
+module.exports = app;
